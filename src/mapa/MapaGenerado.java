944141680bd6165e036cea8a6a5937c29ac425b6
Fredy Sánchez <00082817@uca.edu.sv>
2018-07-01 22:21:44 -0600

    MapaGenerado extiende de Mapa y sobreescibe uno de sus metodos

--- src/mapa/MapaGenerado.java
+++ src/mapa/MapaGenerado.java
@@ -5,10 +5,27 @@
  */
 package mapa;
 
+import java.util.Random;
+
 /**
  *
  * @author fredy
  */
-public class MapaGenerado {
+public class MapaGenerado extends Mapa{
+    
+    private final Random aleatorio = new Random();
+    
+    public MapaGenerado(int ancho, int alto) {
+        super(ancho, alto);
+    }
+    
+    @Override
+    protected void generarMapa(){
+       for(int y = 0; y < alto; y++){
+           for(int x = 0; x < ancho; x++){
+               cuadros[x + y * ancho]= aleatorio.nextInt(3);
+           }
+       } 
+    }
     
 }
