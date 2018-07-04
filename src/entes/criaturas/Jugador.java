a4fa98bf758a77cc5f6bd002df44c62cfb848141
Fredy Sánchez <00082817@uca.edu.sv>
2018-07-04 13:50:51 -0600

    Se incluyó la función de correr en el juego lo cual se hace con la tecla shift

--- src/entes/criaturas/Jugador.java
+++ src/entes/criaturas/Jugador.java
@@ -35,26 +35,33 @@ public class Jugador extends Criatura {
     public void actualizar() {
         int desplazamientoX = 0;
         int desplazamientoY = 0;
+        
+        int velocidadMovimiento = 1;
+        
         if (animacion < 32767){
             animacion++;
         }else{
             animacion=0;
         }
+        
+        if(teclado.correr){
+            velocidadMovimiento =3;
+        }
 
         if (teclado.arriba) {
-            desplazamientoY--;
+            desplazamientoY-= velocidadMovimiento;
         }
 
         if (teclado.abajo) {
-            desplazamientoY++;
+            desplazamientoY+= velocidadMovimiento;
         }
 
         if (teclado.izquierda) {
-            desplazamientoX--;
+            desplazamientoX-= velocidadMovimiento;
         }
 
         if (teclado.derecha) {
-            desplazamientoX++;
+            desplazamientoX+= velocidadMovimiento;
         }
 
         if (desplazamientoX != 0 || desplazamientoY != 0) {            
@@ -63,46 +70,66 @@ public class Jugador extends Criatura {
         }else{
             enMovimiento = false;
         }
+        
+        int resto = animacion % 40;
+        
+        
         if(direccion== 'n'){
                 sprite = Sprite.ARRIBA0;
                 if (enMovimiento){
                     //Velocidad en la que se alternan las animaciones
                     //Se divide entre un numero par y su modulo o residuo debe ser mayor que la mitad de ese numero
                     //porque depende del numero de sprites 
-                    if (animacion % 30 > 15){                        
+                    if (resto > 10 && resto <= 20){
                         sprite = Sprite.ARRIBA1;
-                    }else{
+                    }else if(resto > 20 && resto <= 30){
+                        sprite = Sprite.ARRIBA0;
+                    }else if(resto > 30){
                         sprite = Sprite.ARRIBA2;
+                    }else{
+                        sprite = Sprite.ARRIBA0;
                     }
                 }
             }
             if(direccion== 's'){
                 sprite = Sprite.ABAJO0;
                 if (enMovimiento){
-                    if (animacion % 30 > 15){                        
+                    if (resto > 10 && resto <= 20){
                         sprite = Sprite.ABAJO1;
-                    }else{
+                    }else if(resto > 20 && resto <= 30){
+                        sprite = Sprite.ABAJO0;
+                    }else if(resto > 30){
                         sprite = Sprite.ABAJO2;
+                    }else{
+                        sprite = Sprite.ABAJO0;
                     }
                 }
             }
             if(direccion== 'o'){
                 sprite = Sprite.IZQUIERDA0;
                 if (enMovimiento){
-                    if (animacion % 30 > 15){                        
+                    if (resto > 10 && resto <= 20){
                         sprite = Sprite.IZQUIERDA1;
-                    }else{
+                    }else if(resto > 20 && resto <= 30){
+                        sprite = Sprite.IZQUIERDA0;
+                    }else if(resto > 30){
                         sprite = Sprite.IZQUIERDA2;
+                    }else{
+                        sprite = Sprite.IZQUIERDA0;
                     }
                 }
             }
             if(direccion== 'e'){
                 sprite = Sprite.DERECHA0;
                 if (enMovimiento){
-                    if (animacion % 30 > 15){                        
+                    if (resto > 10 && resto <= 20){
                         sprite = Sprite.DERECHA1;
-                    }else{
+                    }else if(resto > 20 && resto <= 30){
+                        sprite = Sprite.DERECHA0;
+                    }else if(resto > 30){
                         sprite = Sprite.DERECHA2;
+                    }else{
+                        sprite = Sprite.DERECHA0;
                     }
                 }
             }
