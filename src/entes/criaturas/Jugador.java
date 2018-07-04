002e64943899a764f8f7cc35b9a3db9b3acedeaa
Fredy Sánchez <00082817@uca.edu.sv>
2018-07-04 13:20:06 -0600

    Se incluyeron dos nuevas imagenes en el proyecto temporalmente y se modifico la clase jugador para que ahora en todas las direcciones que se mueva el jugador se vea la animacion del movimiento

--- src/entes/criaturas/Jugador.java
+++ src/entes/criaturas/Jugador.java
@@ -16,6 +16,8 @@ import Graficos.Sprite;
 public class Jugador extends Criatura {
 
     private Teclado teclado;
+    
+    private int animacion;
 
     public Jugador(Teclado teclado, Sprite sprite) {
         this.teclado = teclado;
@@ -33,6 +35,11 @@ public class Jugador extends Criatura {
     public void actualizar() {
         int desplazamientoX = 0;
         int desplazamientoY = 0;
+        if (animacion < 32767){
+            animacion++;
+        }else{
+            animacion=0;
+        }
 
         if (teclado.arriba) {
             desplazamientoY--;
@@ -50,21 +57,55 @@ public class Jugador extends Criatura {
             desplazamientoX++;
         }
 
-        if (desplazamientoX != 0 || desplazamientoY != 0) {
-            if(direccion== 'n'){
+        if (desplazamientoX != 0 || desplazamientoY != 0) {            
+            mover(desplazamientoX, desplazamientoY);
+            enMovimiento = true;
+        }else{
+            enMovimiento = false;
+        }
+        if(direccion== 'n'){
                 sprite = Sprite.ARRIBA0;
+                if (enMovimiento){
+                    //Velocidad en la que se alternan las animaciones
+                    //Se divide entre un numero par y su modulo o residuo debe ser mayor que la mitad de ese numero
+                    //porque depende del numero de sprites 
+                    if (animacion % 30 > 15){                        
+                        sprite = Sprite.ARRIBA1;
+                    }else{
+                        sprite = Sprite.ARRIBA2;
+                    }
+                }
             }
             if(direccion== 's'){
                 sprite = Sprite.ABAJO0;
+                if (enMovimiento){
+                    if (animacion % 30 > 15){                        
+                        sprite = Sprite.ABAJO1;
+                    }else{
+                        sprite = Sprite.ABAJO2;
+                    }
+                }
             }
             if(direccion== 'o'){
                 sprite = Sprite.IZQUIERDA0;
+                if (enMovimiento){
+                    if (animacion % 30 > 15){                        
+                        sprite = Sprite.IZQUIERDA1;
+                    }else{
+                        sprite = Sprite.IZQUIERDA2;
+                    }
+                }
             }
             if(direccion== 'e'){
                 sprite = Sprite.DERECHA0;
+                if (enMovimiento){
+                    if (animacion % 30 > 15){                        
+                        sprite = Sprite.DERECHA1;
+                    }else{
+                        sprite = Sprite.DERECHA2;
+                    }
+                }
             }
-            mover(desplazamientoX, desplazamientoY);
-        }
 
     }
     
